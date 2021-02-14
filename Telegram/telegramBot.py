759e2ef0a84446ffaecd39326c84a8ae93500246
Mark Mischcenko <mak-ares@yandex.ru>
2021-02-14 18:50:48 +0300

    Вроде все работает

--- Telegram/telegramBot.py
+++ Telegram/telegramBot.py
@@ -1,30 +1,28 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+import settings
 
 logtime = '%(asctime)s %(message)s'
 logdate = '%m/%d/%Y %I:%M:%S %p'
-logging.basicConfig(filename='Telegram/bot.log', level=20, format=logtime, datefmt=logdate) #Логирование событий бота
+logging.basicConfig(filename='bot.log', level=20, format=logtime, datefmt=logdate) #Логирование событий бота
 
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+PROXY = {'proxy_url': settings.PROXY_URL, 'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSOWRD }}
 
 def greet_user(update, context):
     print('Вызван /start')
     update.message.reply_text('Привет пользователь! Ты вызвал команду СТРАТ')
     print(update)
-    print(1/0)
-
 
 def talk_to_me(update, context):
     text = update.message.text
     print(text)
     update.message.reply_text(text)
-def main():
-    PASS = "1645080260:AAFPS09Z4lN8ShaI-W3p9Jk0H1i_QOdZGJQ"
-    mybot = Updater(PASS, use_context=True, request_kwargs=PROXY)
     
+def main():
+    PASS = settings.API_KEY
+    mybot = Updater(PASS, use_context=True, request_kwargs=PROXY)
+
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
@@ -33,5 +31,6 @@ def main():
 
     mybot.start_polling()
     mybot.idle()
-    
-main()
+
+if __name__ == '__main__':
+    main()
