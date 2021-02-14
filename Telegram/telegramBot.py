84b9461474d698cae0619f09c35a053ddb699654
Mark Mischcenko <mak-ares@yandex.ru>
2021-02-14 17:04:16 +0300

    Бот научился реагировать на команду /start

--- Telegram/telegramBot.py
+++ Telegram/telegramBot.py
@@ -1,2 +1,23 @@
-from telegram.ext import Updater
+import logging
+from telegram.ext import Updater, CommandHandler
 
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет пользователь! Ты вызвал команду СТРАТ')
+    print(update)
+
+def main():
+    PASS = "1645080260:AAFPS09Z4lN8ShaI-W3p9Jk0H1i_QOdZGJQ"
+    mybot = Updater(PASS, use_context=True, request_kwargs=PROXY)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+
+    mybot.start_polling()
+    mybot.idle()
+    
+main()
