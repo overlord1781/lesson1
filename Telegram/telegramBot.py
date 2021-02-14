8e336c33617ac6c9ad2e3c86cf3fa9bfdccbb7ff
Mark Mischcenko <mak-ares@yandex.ru>
2021-02-14 18:13:24 +0300

    доработка

--- Telegram/telegramBot.py
+++ Telegram/telegramBot.py
@@ -1,10 +1,10 @@
 import logging
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 logtime = '%(asctime)s %(message)s'
 logdate = '%m/%d/%Y %I:%M:%S %p'
-logging.basicConfig(filename='bot.log', level=20, format=logtime, datefmt=logdate) #Логирование событий бота
+logging.basicConfig(filename='Telegram/bot.log', level=20, format=logtime, datefmt=logdate) #Логирование событий бота
 
 
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
@@ -16,12 +16,18 @@ def greet_user(update, context):
     print(update)
     print(1/0)
 
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
 def main():
     PASS = "1645080260:AAFPS09Z4lN8ShaI-W3p9Jk0H1i_QOdZGJQ"
     mybot = Updater(PASS, use_context=True, request_kwargs=PROXY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("бот запустился")
 
