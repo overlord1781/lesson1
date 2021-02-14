d74921977928157ddb1b08687139e2a4c5325c4e
Mark Mischcenko <mak-ares@yandex.ru>
2021-02-14 23:05:47 +0300

    Улучшил проверку на чило

--- HomeWork/numbers.py
+++ HomeWork/numbers.py
@@ -1,5 +1,5 @@
-v = int(input('Введите число от 1 до 10 '))
-if v>=1 and v<=10:
+v = (input('Введите число от 1 до 10 '))
+if type(v) == int and v>=1 and v<=10:
     print(v+10)
 else: 
     print('Вы ввели не верное число')
